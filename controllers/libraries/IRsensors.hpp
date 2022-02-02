ceb8a4bedc5f56f82e003283f0cb65b54e6cd7aa
Anna <annalusa202@gmail.com>
2022-02-02 17:21:38 -0300

    IRsensors - Calibrar Sensores
    Adição da etapa de calibração dos sensores, que ocorre antes da corrida.

--- controllers/libraries/IRsensors.hpp
+++ controllers/libraries/IRsensors.hpp
@@ -10,7 +10,7 @@ pela identificação da posição do robô e leitura das marcações da pista*/
 #include <iostream>
 #include "stdio.h"
 
-#define QTDE_SENSORES 8 //presto possuim 8 sensores
+#define SENSORES_SETUP 20 
 #define DIST_TRIGGER 3 //cte para processamento da distancia
 
 using namespace webots;
@@ -18,39 +18,97 @@ using namespace std;
 
 string sensor_name[] = {"frontal_sensors", "right_sensor", "left_right_sensor", "left_left_sensor"};
 
-class IRsensor
-{
+class IRsensor{
     private:
     public:
 
+    int qtde_sensores;
+    int index_da_leitura = 0;
+    int ultima_leitura = 0;
     //vetores que armazenam os valores max e min para o calibrate:
         double max[20];
         double min[20];
 
-    
-}
-//ANTES DA CORRIDA
-    //Calibrar Sensores
-    void CALIBRATE(){
-        while (robot->step(TIMESTEP) != -1){
+    DistanceSensor *sensores[20];
 
-            double time = robot->getTime();
+    IRsensor(int _quantidade_sensores, string _nome_dos_sensores[], Robot *meu_robo)
+    {
 
-            while (time < 11){
-                robot->(TIMESTEP);
-                right_sensor.calibrateSensors(time);
-                left_right_sensor.calibrateSensors(time);
-                left_left_sensor.calibrateSensors(time);
-                frontal_sensor.calibrateSensors(time);
+        qtde_sensores = _quantidade_sensores;
+    //instanciação dos sensores
+        if(qtde_sensores = 4){
+            
+            DistanceSensor *right_sensor = robot->getDistanceSensor("ir_right");
+            right_sensor -> enable(TIMESTEP);
+            sensores[0] = right_sensor;
 
-                time = robot->getTime();
-            }
-            cout << "Fim da calibracao" << endl;
+            DistanceSensor *left_right_sensor = robot->getDistanceSensor("ir_left_right");
+            left_right_sensor -> enable(TIMESTEP);
+            sensores[1] = left_right_sensor;
+
+            DistanceSensor *left_left_sensor = robot->getDistanceSensor("ir_left_left");
+            left_left_sensor -> enable(TIMESTEP);
+            sensores[2] = left_left_sensor;
+
+            DistanceSensor *frontal_sensor = robot->getDistanceSensor("ir4");
+            frontal_sensor -> enable(TIMESTEP);
+            sensores[3] = frontal_sensor;
+
+
+            maximo[4]= {0, 0, 0, 0};
+            minimo[4]={100000, 100000, 100000, 100000};
         }
+        else{
+            for(int i = 0; i < qtde_sensores; i++)
+            {
+                sensores[i] = meu_robo->getDistanceSensor(_nome_dos_sensores[i]);
+                sensores[i]->enable(TIMESTEP);
+                maximo[i]=0;
+                minimo[i]=100000; 
+            }
+        }   
+
+        //ANTES DA CORRIDA
+            //Calibrar Sensores por 11seg
+            void CALIBRARSENSORS(){
+                while (robot->step(TIMESTEP) != -1){
+
+                    double time = robot->getTime();
+
+                    while (time < 11){
+                        robot->(TIMESTEP);
+                        right_sensor.calibrateSensors(time);
+                        left_right_sensor.calibrateSensors(time);
+                        left_left_sensor.calibrateSensors(time);
+                        frontal_sensor.calibrateSensors(time);
+
+                        time = robot->getTime();
+                    }
+                    cout << "Fim da calibracao" << endl;
+                }
+            }
+            //Ler valores calibrados
+            void LERSENSORS{
+                for(int ind_sensor; ind_sensor < qtde_sensores; ind_sensor++){
+                    sensores[ind_sensor] -> getValue();
+                   int medias[ind_sensor] = getMedian(qtde_sensores, sensores[qtde_sensores]);
+                }
+                //atualiza para última posição de leitura
+                index_da_leitura ++;
+
+                //loop da leitura de 0 a 19
+                if(index_da_leitura == 20)
+                    index_da_leitura = 0;
+                            
+            }
+        //DURANTE A CORRIDA
+            //Qual a posição do Robô?
+            //Ler Marcações da Pista
+        
     }
-    //Ler valores calibrados
-//DURANTE A CORRIDA
-    //Qual a posição do Robô?
-    //Ler Marcações da Pista
+
+} 
+
+
 
 #endif
