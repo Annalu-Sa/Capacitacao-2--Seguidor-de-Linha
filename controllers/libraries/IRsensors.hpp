14cf2e0f9454d1da22d06cd0921df7d80a7aab32
Anna <annalusa202@gmail.com>
2022-02-08 13:26:49 -0300

    Finalizado: Capinar Função IRsensors
    Devido às circunstâncias, a capacitação 2 sofreu alterações e passamos a ter que capinar, cada membro e membra, a sua respectiva função do código base.

--- controllers/libraries/IRsensors.hpp
+++ controllers/libraries/IRsensors.hpp
@@ -3,14 +3,14 @@ pela identificação da posição do robô e leitura das marcações da pista*/
 
 #ifndef IRSENSORS_HPP
 #define IRSENSORS_HPP
-
+//CAPINAR O CODIGO IRSENSORS
 #include "funcoes_uteis.hpp"
 #include "C:/Program Files/Webots/include/controller/cpp/webots/DistanceSensor.hpp"
 #include "C:/Program Files/Webots/include/controller/cpp/webots/Robot.hpp"
 #include <iostream>
 #include "stdio.h"
 
-#define SENSORES_SETUP 20 
+#define QTDE_SENSORES_SETUP 15 
 #define DIST_TRIGGER 3 //cte para processamento da distancia
 
 using namespace webots;
@@ -18,16 +18,20 @@ using namespace std;
 
 string sensor_name[] = {"frontal_sensors", "right_sensor", "left_right_sensor", "left_left_sensor"};
 
-class IRsensor{
+class IRsensor
+{
     private:
     public:
 
     int qtde_sensores;
+    double leituras_calibradas[QTDE_SENSORES_SETUP];
     int index_da_leitura = 0;
     int ultima_leitura = 0;
-    //vetores que armazenam os valores max e min para o calibrate:
-        double max[20];
-        double min[20];
+    double medias[QTDE_SENSORES_SETUP];
+
+    //Criacao dos vetores que armazenam os valores max e min para o calibrate:
+    double maximo[20] = VECcreator(20; maximo[20]);
+    double minimo[20] = VECcreator(20; minimo[20]);
 
     DistanceSensor *sensores[20];
 
@@ -36,79 +40,161 @@ class IRsensor{
 
         qtde_sensores = _quantidade_sensores;
     //instanciação dos sensores
-        if(qtde_sensores = 4){
-            
-            DistanceSensor *right_sensor = robot->getDistanceSensor("ir_right");
-            right_sensor -> enable(TIMESTEP);
-            sensores[0] = right_sensor;
-
-            DistanceSensor *left_right_sensor = robot->getDistanceSensor("ir_left_right");
-            left_right_sensor -> enable(TIMESTEP);
-            sensores[1] = left_right_sensor;
-
-            DistanceSensor *left_left_sensor = robot->getDistanceSensor("ir_left_left");
-            left_left_sensor -> enable(TIMESTEP);
-            sensores[2] = left_left_sensor;
-
-            DistanceSensor *frontal_sensor = robot->getDistanceSensor("ir4");
-            frontal_sensor -> enable(TIMESTEP);
-            sensores[3] = frontal_sensor;
-
-
-            maximo[4]= {0, 0, 0, 0};
-            minimo[4]={100000, 100000, 100000, 100000};
-        }
-        else{
-            for(int i = 0; i < qtde_sensores; i++)
+        for(int i = 0; i < qtde_sensores; i++)
+        {
+            sensores[i] = meu_robo->getDistanceSensor(_nome_dos_sensores[i]);
+            sensores[i]->enable(TIMESTEP);
+            maximo[i]=0;
+            minimo[i]=100000; 
+        } 
+    }
+    //ANTES DA CORRIDA
+    
+    //Calibrar Sensores por 11seg
+    void CALIBRARSENSORS(double time)
+    {
+                
+        //Esses 3seg é um delay para evitar ruídos no Webots
+        if(time > 3)
+        {
+            while (robot->step(TIMESTEP) != -1)
             {
-                sensores[i] = meu_robo->getDistanceSensor(_nome_dos_sensores[i]);
-                sensores[i]->enable(TIMESTEP);
-                maximo[i]=0;
-                minimo[i]=100000; 
-            }
-        }   
+                double time = robot->getTime();
 
-        //ANTES DA CORRIDA
-            //Calibrar Sensores por 11seg
-            void CALIBRARSENSORS(){
-                while (robot->step(TIMESTEP) != -1){
-
-                    double time = robot->getTime();
-
-                    while (time < 11){
-                        robot->(TIMESTEP);
-                        right_sensor.calibrateSensors(time);
-                        left_right_sensor.calibrateSensors(time);
-                        left_left_sensor.calibrateSensors(time);
-                        frontal_sensor.calibrateSensors(time);
-
+                while (time < 11)
+                {
+                    for(int i = 0; i < qtde_sensores; i++)
+                    {
                         time = robot->getTime();
+                        sensores[i].calibrateSensors(time);    
                     }
-                    cout << "Fim da calibracao" << endl;
                 }
+                cout << "Fim da calibracao" << endl;
             }
-            //Ler valores calibrados
-            void LERSENSORS{
-                for(int ind_sensor; ind_sensor < qtde_sensores; ind_sensor++){
-                    sensores[ind_sensor] -> getValue();
-                   int medias[ind_sensor] = getMedian(qtde_sensores, sensores[qtde_sensores]);
-                }
-                //atualiza para última posição de leitura
-                index_da_leitura ++;
+        }
+    }
+    //Leitura dos sensores sem a limitacao 'constrain'
+    void LERSENSORS_puro()
+    {
 
-                //loop da leitura de 0 a 19
-                if(index_da_leitura == 20)
-                    index_da_leitura = 0;
+        //Lendo os valores capturados por cada sensor
+        for(int ind_sensor = 0; ind_sensor < qtde_sensores; ind_sensor++)
+        {
+            sensores[ind_sensor] -> getValue();
+                
+            //Fazendo a media desses valores capturados
+            int VECintermedio[ind_sensor][index_da_leitura] = getMedian(qtde_sensores, sensores[qtde_sensores]);
+
+            medias[ind_sensor] = VECintermedio[ind_sensor][index_da_leitura];
+        }
+               
+        //Atualiza para ultima posicao de leitura
+        index_da_leitura ++;
+
+        //loop da leitura de 0 a 19
+        if(index_da_leitura == 20)
+            index_da_leitura = 0;
                             
-            }
-        //DURANTE A CORRIDA
-            //Qual a posição do Robô?
-            //Ler Marcações da Pista
-        
     }
 
-} 
+    //função auxiliar pro calibrate sensors
+    void AUX_CALIBRATE_SENSORS()
+    {
+        //Comparar valor_medio,que pegamos no LERSENSORS_puro, e definir um maximo/minimo
+        //pra cada sensor. 
+        for (int i = 0; i < qtde_sensores; i++)
+        { 
+            if (medias[i] < minimo[i]) minimo[i] = medias[i];
+            if (medias[i] > maximo[i]) maximo[i] = medias[i];
+        }
+    }
 
+    //Leitura dos Sensores com a limitação do 'constrain', que servira para termos certeza que
+    //os valores que queremos, foram, de fato, limitados
+    double LERSENSORS_lim()
+    {
+        for (int i = 0; i<qtde_sensores; i++)
+        { 
+            leituras_calibradas[i] = constrain(map(medias[i], minimo[i], maximo[i], 0, 1000), 0, 1000);
+        }
+        if (qtde_sensores == 1) 
+            return leituras_calibradas[0];
+        else 
+            return -1;
 
+    }
+    //DURANTE A CORRIDA
+    
+    //Qual a posicao do Robo?(pegar posicao com metodo ReadLine)
+    double readLine()
+    {
+        unsigned char i;
+        unsigned long avg; // Isso é para o peso total, que é long
+                            // Antes da divisão
+        unsigned int sum; // Isso é para o denominador, que é <= 64000
+
+        readCalibrated();   //readLine trabalha com as leituras calibradas de 0 a 1000
+
+        avg = 0;
+        sum = 0;
+        int on_line = 0;
+
+        for(i=0; i<qtde_sensores; i++)
+        {
+            double leitura = leituras_calibradas[i];
+            if(leitura > 600) on_line = 1;        //Valor arbitrario, verificar durante simulacao!
+            if(leitura > 250)
+            {
+                avg += (long)(leitura) * (i * 1000);
+                sum += leitura;
+            }
+        }
+        if (!on_line) //Se o robo nao estiver vendo a linha 
+        {
+            //Se ele sair da linha pela direita, retorna o valor minimo pra reencontrar a linha na esquerda
+             if(ultima_leitura < (qtde_sensores-1)*1000/2)
+                return 0;
+            //Se ele sair da linha pela esquerda, retorna o valor máximo pra reencontrar a linha na direita
+             else
+                return (qtde_sensores-1)*1000;
+        }
+        if(sum != 0) 
+            ultima_leitura = avg/sum;
+        else 
+            ultima_leitura = 0;
+        return ultima_leitura;
+    }      
+};
+//Ler Marcações Direita e Esquerda da Pista
+
+double last_giro_esq, last_giro_dir = 0;
+
+//Usado para ler as marcações no lado esquerdo da pista e sinalizar as curvas.
+bool readLeft(double giro_esq_atual, double giro_dir_atual, double leitura_esq_dir,  double leitura_esq_esq, int mapping_path)
+{
+
+    if (((leitura_esq_dir > 700) || (leitura_esq_esq > 700))      // está vendo a marcação
+        && giro_esq_atual - last_giro_esq > DIST_TRIGGER && giro_dir_atual - last_giro_dir > DIST_TRIGGER)
+    {   
+        // Passou pelo menos 3 cm da marcação anterior
+        last_giro_esq = giro_esq_atual;
+        last_giro_dir = giro_dir_atual;
+        mapping_path += 1;
+        return true;
+    }
+    return false;
+};
+
+bool readRight(double giro_esq_atual, double giro_dir_atual, double leitura_dir, int mapping_path)
+{
+    if ((leitura_dir > 700) && (giro_esq_atual - last_giro_esq) > DIST_TRIGGER && (giro_dir_atual - last_giro_dir) > DIST_TRIGGER)
+    {
+        last_giro_esq = giro_esq_atual;
+        last_giro_dir = giro_dir_atual;
+        mapping_path += 1;
+        return true;
+    }
+    return false;
+};
 
 #endif
